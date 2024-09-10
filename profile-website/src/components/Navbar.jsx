8e722327f2d15433573241a61345fe463687551e
web3Edtech <webonwardsedtech@gmail.com>
2024-09-10 06:45:23 +0530

    Update Navbar.jsx

--- profile-website/src/components/Navbar.jsx
+++ profile-website/src/components/Navbar.jsx
@@ -4,12 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/profile">Profiles</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+           {/* logic to switch between pages */}
         </nav>
     );
 };
